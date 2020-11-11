43e634f3005dcb314b4363fdb0d70f721f9b0883
jamesledev <74250657+jamesledev@users.noreply.github.com>
2020-11-11 15:47:12 +0000

    Added first type

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -3,39 +3,51 @@ var router = express.Router();
 
 const axios = require('axios');
 /* GET home page. */
-router.get('/:id', function(req, res, next) {
-    var id = req.params.id;
-    console.log(id);
-    var url ='https://pokeapi.co/api/v2/pokemon/'+ id;
+router.get('/:id', function (req, res, next) {
+  var id = req.params.id;
+  console.log(id);
+  var url = 'https://pokeapi.co/api/v2/pokemon/' + id;
 
 
-  
 
-axios.get(url)
-  .then(function (response) {
- 
-    var pokemonObject = {
+
+  axios.get(url)
+
+    .then(function (response) {
+
+      var pokemonObject = {
+        id: response.data.id,
         name: response.data.name,
         weight: response.data.weight / 10,
         height: response.data.height / 10,
-        img: response.data.sprites.front_default
-    }
-    return pokemonObject
-  })
-  .catch(function (error) {
-    
-    console.log('look here james here is the error', error);
-  })
-  .then(function (pokemonObject) {
+        img: response.data.sprites.front_default,
+        type1: response.data.types[0].type.name,
+      
+      }
+      // if (response.data.types.length == 2) {
+      // type2  response.data.types[1].type.name;
+      // } else {type2 = "";
+    // } 
+      return pokemonObject
+    })
+
+
+
+    .catch(function (error) {
+
+      console.log('look here james here is the error', error);
+    })
+    .then(function (pokemonObject) {
       console.log(pokemonObject)
 
 
-    res.render('pokemon', pokemonObject);
-  });
-  
- 
+      res.render('pokemon', pokemonObject);
+    });
+
+
 });
 
 
 
 module.exports = router;
+
