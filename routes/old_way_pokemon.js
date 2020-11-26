ab658fcfbf63c431c7f783b32822a4a61d97e9e2
jamesledev <74250657+jamesledev@users.noreply.github.com>
2020-11-26 18:43:26 +0000

    Evolutions
    evolutions with levels finally added

--- routes/old_way_pokemon.js
+++ routes/old_way_pokemon.js
@@ -7,16 +7,15 @@ router.get('/:id', function (req, res, next) {
   const id = req.params.id;
   console.log(id);
   const url = 'https://pokeapi.co/api/v2/pokemon/' + id;
-
+  let pokemonObject = {};
   axios
     .get(url)
-
     .then(function (response) {
       let type2 = '';
       if (response.data.types[1]) {
         type2 = response.data.types[1].type.name;
       }
-      const pokemonObject = {
+      pokemonObject = {
         id: response.data.id,
         name: response.data.name,
         weight: response.data.weight / 10,
@@ -25,17 +24,57 @@ router.get('/:id', function (req, res, next) {
         type1: response.data.types[0].type.name.toLowerCase(),
         type2: type2.toLowerCase(),
       };
-      console.log(response.data);
+      // console.log(speciesUrl);
+      return axios.get('https://pokeapi.co/api/v2/pokemon-species/' + id);
+    })
+    .then(function (response) {
+      let evoUrl = response.data.evolution_chain.url;
+      return axios.get(evoUrl);
+    })
+    .then(function (response) {
+      pokemonObject.evolutions = [];
+      pokemonObject.evolutions.push({
+        name: response.data.chain.species.name,
+        level: '',
+      });
+      if (response.data.chain.evolves_to[0]) {
+        for (var i = 0; i < response.data.chain.evolves_to.length; i++) {
+          let evoTwo = response.data.chain.evolves_to[i].species.name;
+          let level =
+            response.data.chain.evolves_to[i].evolution_details[0].min_level;
+          const evolutionTree = {
+            name: evoTwo,
+            level: level,
+          };
+          pokemonObject.evolutions.push(evolutionTree);
+          console.log(level);
+        }
+      }
+      if (response.data.chain.evolves_to[0].evolves_to[0]) {
+        for (var i = 0; i < response.data.chain.evolves_to.length; i++) {
+          let evoThree =
+            response.data.chain.evolves_to[0].evolves_to[i].species.name;
+          let level =
+            response.data.chain.evolves_to[0].evolves_to[i].evolution_details[0]
+              .min_level;
+          const evolutionTree = {
+            name: evoThree,
+            level: level,
+          };
+          pokemonObject.evolutions.push(evolutionTree);
+        }
+      }
+
+      //level
+      //item
+      //condition
+
       return pokemonObject;
     })
-    // .then to get the url
-
-    //chaining api requests
-
     .catch(function (error) {
       console.log('look here james here is the error', error);
     })
-    .then(function (pokemonObject) {
+    .then(function (data) {
       console.log(pokemonObject);
       res.render('pokemon', pokemonObject);
     });
