9b1c5198fb79ad81d79cf7eec5f0107b290ef94d
jamesledev <74250657+jamesledev@users.noreply.github.com>
2020-12-08 23:20:42 +0000

    Everything
    Added everything

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 //pokemon router
-var pokemonRouter = require('./routes/old_way_pokemon');
+var pokemonRouter = require('./routes/pokemon');
+var pokemonSearchRouter = require('./routes/pokemon-search');
 
 var app = express();
 
@@ -23,15 +24,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/pokemon', pokemonRouter);
+app.use('/pokemon-search', pokemonSearchRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -2,42 +2,153 @@ var express = require('express');
 var router = express.Router();
 
 const axios = require('axios');
-/* GET home page. */
-router.get('/:id', async function (req, res, next) {
-  var id = req.params.id;
-  console.log(id);
-  var url = 'https://pokeapi.co/api/v2/pokemon/' + id;
+const { getIDFromURL } = require('../utils/index');
 
-
-  const response = await axios.get(url);
-  
-  // const evolution = await axios.get(asdasdasdasdasdasd)
-
-  var type2 = null
-  if(response.data.types[1]){
-    type2 = response.data.types[1].type.name;
-  }  else var type2 = "";
-
-  var pokemonObject = {
-    id: response.data.id,
-    name: response.data.name,
-    weight: response.data.weight / 10,
-    height: response.data.height / 10,
-    img: response.data.sprites.front_default,
-    type1: response.data.types[0].type.name,
-    type2: type2 
-
+function getEvolutionInformation(evolutionChain) {
+  const evolutionArray = [];
+  for (var i = 0; i < evolutionChain.length; i++) {
+    const evolutionTo = evolutionChain[i];
+    const evoTwo = evolutionTo.species.name;
+    const level = evolutionTo.evolution_details[0].min_level;
+    const id = getIDFromURL(evolutionTo.species.url);
+    const evolutionDetails = evolutionTo.evolution_details[0];
+    const trigger =
+      evolutionTo.evolution_details[0].trigger.name != 'trade'
+        ? null
+        : evolutionTo.evolution_details[0].trigger.name;
+    let {
+      gender,
+      held_item,
+      item,
+      known_move,
+      known_move_type,
+      location,
+      min_affection,
+      min_beauty,
+      min_happiness,
+      needs_overworld_rain,
+      party_species,
+      party_type,
+      relative_physical_stats,
+      time_of_day,
+      turn_upside_down,
+    } = evolutionDetails;
+    item = item ? item.name : item;
+    location = location ? location.name : location;
+    known_move_type = known_move_type ? known_move_type.name : known_move_type;
+    known_move = known_move ? known_move.name : known_move;
+    held_item = held_item ? held_item.name : held_item;
+    if (relative_physical_stats === 0) {
+      relative_physical_stats = 'equal';
+    }
+    const evolutionTree = {
+      name: evoTwo,
+      level,
+      id,
+      gender,
+      held_item,
+      item,
+      known_move,
+      known_move_type,
+      location,
+      min_affection,
+      min_beauty,
+      min_happiness,
+      needs_overworld_rain,
+      party_species,
+      party_type,
+      relative_physical_stats,
+      time_of_day,
+      turn_upside_down,
+      trigger,
+    };
+    evolutionArray.push(evolutionTree);
   }
+  return evolutionArray;
+}
 
-
-
-  res.render('pokemon', pokemonObject);
-
-
-
+router.get('/', function (req, res, next) {
+  res.redirect('/pokemon/1');
 });
 
+router.get('/:id', async function (req, res, next) {
+  try {
+    // console.log(req);
+    const id = req.params.id;
 
+    console.log(id);
+    const url = 'https://pokeapi.co/api/v2/pokemon/' + id;
+
+    const response = await axios.get(url);
+    const evolution = await axios.get(
+      'https://pokeapi.co/api/v2/pokemon-species/' + id
+    );
+    const evoUrl = await evolution.data.evolution_chain.url;
+    const evoInfo = await axios.get(evoUrl);
+    const evolutionChain = evoInfo.data.chain;
+
+    // let type2 = null;
+    // if (response.data.types[1]) {
+    //   type2 = response.data.types[1].type.name;
+    // } for reference of this below if statement
+    const type2 = response.data.types[1]
+      ? response.data.types[1].type.name
+      : null;
+    const firstEvo = [];
+    firstEvo.push({
+      name: evolutionChain.species.name,
+      level: '',
+      id: getIDFromURL(evolutionChain.species.url),
+    });
+
+    const secondEvolutionChain = evolutionChain.evolves_to.length
+      ? evolutionChain.evolves_to
+      : null;
+    const secondEvo = secondEvolutionChain
+      ? getEvolutionInformation(secondEvolutionChain)
+      : [];
+
+    const thirdEvolutionChain =
+      secondEvolutionChain && secondEvolutionChain[0].evolves_to.length
+        ? evolutionChain.evolves_to[0].evolves_to
+        : null;
+
+    const thirdEvo = thirdEvolutionChain
+      ? getEvolutionInformation(thirdEvolutionChain)
+      : [];
+
+    const pokemonObject = {
+      id: response.data.id,
+      name: response.data.name,
+      weight: response.data.weight / 10,
+      height: response.data.height / 10,
+      img: response.data.sprites.other['official-artwork'].front_default,
+      type1: response.data.types[0].type.name,
+      type2: type2,
+      evolutions: {
+        firstEvo,
+        secondEvo,
+        thirdEvo,
+      },
+    };
+    //catch error
+
+    res.render('pokemon', pokemonObject);
+  } catch (error) {
+    const message =
+      error.response && error.response.status === 404
+        ? 'Pokemon not found'
+        : 'Generic Error';
+    res.render('error', {
+      message,
+      error: {
+        stack: error.stack,
+        status:
+          error.response && error.response.status ? error.response.status : '',
+      },
+    });
+    // console.log('look here james here is the error', error);
+  }
+});
 
 module.exports = router;
-
