3cd03c24a99d9a5a3bf50ac626efad99bc6beef3
jamesledev <74250657+jamesledev@users.noreply.github.com>
2021-01-01 22:49:48 +0000

    finalising css

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   const object = {
     title: 'Express',
-    name: 'James'
-  }
+    name: 'James',
+  };
   res.render('index', object);
 });
 
-router.get('/james', function(req, res, next) {
+router.get('/james', function (req, res, next) {
   const object = {
     title: 'Express',
-    name: 'Le'
-  }
+    name: 'Le',
+  };
   res.render('james', object);
+});
+
+router.post('/', function (req, res, next) {
+  res.redirect(`/pokemon/1`);
 });
 
 module.exports = router;
